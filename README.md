351832ec8fb2d242384c734772093385a40551a5
Thangapandi-K <thangapandi.chella@gmail.com>
2024-07-24 12:49:52 +0530

    file system --read files in the folder

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 - Content of the file will be the current timestamp.
 
 - The file name should be current date-time.txt.
+
+### Endpoint: `/newFile`
+
+
+> 2. Writing an API endpoint to retrieve all the text files names in that particular folder.
+
+### Endpoint: `/getFiles`
--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.use(express.json());
 
 app.get('/newFile', (request, response) => {
 
-    // new file content
+// new file content
 let content = `${new Date()}`
 
-    // new file name
+// new file name
 let date = new Date().getDate();
 let month = new Date().getMonth() + 1;
 let year = new Date().getFullYear();
@@ -37,16 +37,37 @@ function fileName() {
     return  `${date}-${month}-${year}-${hour}-${minute}-${seconds}.txt`
 }
 
+
     fs.writeFile('./newFiles/' + fileName() , content, (error) => {
+        
         if(!error) {
             response.send('New File Created Sucessfully !')
           } else {
             response.send('Error')
           }
+
     })
 
 })
 
+
+// '/getFiles' API end point to read file names in the folder
+
+app.get('/getFiles', (request, response) => {
+
+    const folderPath = './newFiles'
+
+    fs.readdir(folderPath, 'utf-8', (error, files) => {
+        
+        if(!error) {
+            response.send(files);
+        } else {
+            response.send('Error Reading File !')
+        } 
+
+    }) 
+
+})
 
 // server
 
